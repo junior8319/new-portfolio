64cf1eb3164b770c99e6f563d3b7b3cb362917e5
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-08-11 09:47:41 -0300

    backend - feat - '/upload' - set route and function.

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('multer');
 const cors = require('cors');
 const {
   stacksRouter,
@@ -7,10 +8,26 @@ const {
 } = require('../routes/index.routes');
 const app = express();
 
+app.use(express.json()); // parse json bodies
 app.use(cors()); // allow all origins
 
 app.use('/stacks', stacksRouter);
 app.use('/projects', projectsRouter);
 app.use('/stacks-projects', stacksProjectsRouter);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../frontend/src/assets/images');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post('/upload', upload.single('snapshot'), (req, res) => {
+  res.send('file uploaded');
+});
 
 module.exports = app;
