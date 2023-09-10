060c7b6f4e4ff11a9bca37a38956a1773110157c
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-09-10 10:57:22 -0300

    backend - feat - login - sets token functions.

---
+++ backend/src/helpers/jsonWebToken.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+
+const secretKey = fs.readFileSync('jwt.evaluation.key', 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+  return data;
+});
+
+const generateToken = async (user) => {
+  const token = await jwt.sign(user, secretKey, { expiresIn: '24h' });
+  console.log(token);
+
+  return token;
+};
+
+const verifyToken = async (token) => {
+  const decoded = await jwt.verify(token, secretKey);
+  console.log(decoded);
+
+  return decoded;
+};
+
+module.exports = {
+  generateToken,
+  verifyToken,
+};
