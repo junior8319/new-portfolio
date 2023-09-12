976b39cacd97a8b83416546a1ce9a49f54b8e7a7
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-09-12 14:22:43 -0300

    backend - feat - login - provides token and login validations to login routes.

--- backend/src/routes/login.routes.js
+++ backend/src/routes/login.routes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const loginController = require('../controllers/Login.controller');
+const { loginValidate } = require('../middlewares/login.validate.middleware');
+const { tokenValidate } = require('../middlewares/token.validate.middleware');
 
 const loginRouter = express.Router();
 
 loginRouter.get('/', loginController.getUsers);
 loginRouter.get('/:id', loginController.getUserById);
-loginRouter.post('/', loginController.createUser);
-loginRouter.post('/login', loginController.login);
-loginRouter.put('/:id', loginController.updateUser);
-loginRouter.delete('/:id', loginController.deleteUser);
+loginRouter.post('/', tokenValidate, loginValidate, loginController.createUser);
+loginRouter.post('/login', loginValidate, loginController.login);
+loginRouter.put('/:id', tokenValidate, loginController.updateUser);
+loginRouter.delete('/:id', tokenValidate, loginController.deleteUser);
 
 module.exports = {
   loginRouter,
