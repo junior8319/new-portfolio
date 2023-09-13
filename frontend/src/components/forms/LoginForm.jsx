f4e62eee233963b89f9c701d789ca674740d2b9d
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-09-13 08:04:33 -0300

    frontend - feat. - LoginForm: sets login buttons and login requests.

--- frontend/src/components/forms/LoginForm.jsx
+++ frontend/src/components/forms/LoginForm.jsx
@@ -1,15 +1,61 @@
-import { FormContainer, FormDiv25 } from "../../styled/Form";
+import { useContext, useState } from "react";
+import { FormContainer, FormDiv100 } from "../../styled/Form";
 import { Input } from "../../styled/Inputs";
 import { Label } from "../../styled/Labels";
 import { Title2 } from "../../styled/Titles";
+import { SaveButton } from "../../styled/Buttons";
+import { requestLogin } from "../../helpers/loginApi";
+import { LoginContext } from "../../context/Contexts";
 
 const LoginForm = () => {
+  const blankForm = {
+    userName: '',
+    password: '',
+  };
+
+  const [user, setUser] = useState(blankForm);
+  const { setIsLogged } = useContext(LoginContext);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const sendLoginRequest = async (event) => {
+    event.preventDefault();
+    const response = await requestLogin(user);
+
+    if (response) {
+      console.log(response);
+      setUser(blankForm);
+      setIsLogged(true);
+      return response;
+    }
+  };
+
+  const visitorLogin = async (event) => {
+    event.preventDefault();
+    setUser({
+      userName: 'visitor',
+      password: 'visitorSuper',
+    });
+    
+    const response = await sendLoginRequest(event);
+
+    if (response) {
+      console.log(response);
+      setUser(blankForm);
+      setIsLogged(true);
+      return response;
+    }
+  };
+
   return (
     <>
       <Title2>Login</Title2>
 
       <FormContainer>
-        <FormDiv25>
+        <FormDiv100>
           <Label
             htmlFor="userName"
           >
@@ -20,12 +66,12 @@ const LoginForm = () => {
             id="userName"
             name="userName"
             type="text"
-            value={''}
-            onChange={''}
+            value={ user.userName }
+            onChange={ handleChange }
           />
-        </FormDiv25>
+        </FormDiv100>
 
-        <FormDiv25>
+        <FormDiv100>
           <Label
             htmlFor="password"
           >
@@ -36,10 +82,22 @@ const LoginForm = () => {
             id="password"
             name="password"
             type="password"
-            value={''}
-            onChange={''}
+            value={ user.password }
+            onChange={ handleChange }
           />
-        </FormDiv25>
+        </FormDiv100>
+
+        <SaveButton
+          type="button"
+          value='Entrar'
+          onClick={ sendLoginRequest }
+        />
+
+        <SaveButton
+          type="button"
+          value='Entrar como Visitante'
+          onClick={ visitorLogin }
+        />
       </FormContainer>
     </>
   );
