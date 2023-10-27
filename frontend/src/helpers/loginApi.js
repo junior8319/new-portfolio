0d4642a7d65d9820a29b57d0531c87d5d546464d
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-10-27 07:37:19 -0300

    frontend - feat. - requestGetUsers(),  requestCreateUser(),  requestDeleteUser().

--- frontend/src/helpers/loginApi.js
+++ frontend/src/helpers/loginApi.js
@@ -1,5 +1,6 @@
 const API_URL = process.env.REACT_APP_BASE_URL;
-const API_ORIGIN = process.env.REACT_APP_BASE_URL_ORIGIN; 
+const API_ORIGIN = process.env.REACT_APP_BASE_URL_ORIGIN;
+const TOKEN = localStorage.getItem('token');
 
 const requestLogin = async (receivedCredencials) => {
   try {
@@ -27,4 +28,88 @@ const requestLogin = async (receivedCredencials) => {
   }
 };
 
-export { requestLogin };
+const requestGetUsers = async () => {
+  try {
+    const options = {
+      method:'GET',
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin': API_ORIGIN,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      }, 
+    };
+
+    const response = await fetch(`${API_URL}/users`, options);
+    const jsonResponse = await response.json();    
+
+    return jsonResponse;
+  } catch (error) {
+    console.log(error);
+    return new Error(`Something went wrong. Error: ${error}`);
+  }
+};
+
+const requestCreateUser = async (receivedUser, receivedCredentials) => {
+  try {
+    const userToRegister = {
+      ...receivedUser,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+  
+    const registerOptions = {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': API_ORIGIN,
+        'Accept': 'application/json',
+        Authorization: receivedCredentials,
+      },
+      body: JSON.stringify(userToRegister),
+    };
+  
+    const registerResponse = await fetch(
+      `${API_URL}/users/create`,
+      registerOptions
+    );
+  
+    const jsonResponse = await registerResponse.json();
+
+    return jsonResponse;    
+  } catch (error) {
+    console.log(error);
+    return new Error(`Something went wrong. Error: ${error}`);
+  }
+};
+
+const requestDeleteUser = async (id) => {
+  try {
+    const otpions = {
+      method:'DELETE',
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin': API_ORIGIN,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': TOKEN,
+      }, 
+    };
+
+    const response = await fetch(`${API_URL}/users/${id}`, otpions);
+    const jsonResponse = await response.json();
+
+    return jsonResponse;
+  } catch (error) {
+    console.log(error);
+    return new Error(`Something went wrong. Error: ${error}`);
+  }
+};
+
+export {
+  requestLogin,
+  requestGetUsers,
+  requestDeleteUser,
+  requestCreateUser,
+};
