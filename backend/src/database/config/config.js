ed9411d373e0a2c23509b446e77306dfc9ded958
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-11-18 16:42:20 -0300

    backend - deploy feat. - updates environment variables.

--- backend/src/database/config/config.js
+++ backend/src/database/config/config.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
     database: 'portfolio_dev',
-    host: process.env.HOSTNAME,
+    host: process.env.MYSQLHOST,
     dialect: 'mysql'
   },
   test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
     database: 'portfolio_test',
-    host: process.env.HOSTNAME,
+    host: process.env.MYSQLHOST,
     dialect: 'mysql'
   },
   production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: 'portfolio_prod',
-    host: process.env.HOSTNAME,
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
     dialect: 'mysql'
   }
 }
